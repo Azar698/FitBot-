0eeef528289c0af08cc27a8ca763678ea4d9b5ac
Shaik Mohammad Azaruddin <143438233+Azar698@users.noreply.github.com>
2025-03-09 11:30:26 +0530

    succesfully files added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = "Genrative AI Project",
+    version="0.00",
+    author="Azar",
+    author_email="mohammadazaruddin6302@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
